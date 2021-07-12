077d155ef46ecb0d07fda1e153ed22adeacc4349
Yrobot <yrobot@qq.com>
2021-07-12 18:28:30 +0800

    测试@generated/type-graphql

--- server.js
+++ server.js
@@ -1,56 +1,59 @@
-const { PrismaClient } = require('@prisma/client');
-const express = require('express');
-const bodyParser = require('body-parser');
+const {} = require('@generated/type-graphql');
+const { ApolloServer, gql } = require('apollo-server');
 
-const prisma = new PrismaClient();
-const app = express();
-app.use(bodyParser());
 const port = 3000;
 
-app.get('/users', async function (req, res) {
-  const users = await prisma.user.findMany();
-  res.send(users);
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen({ port }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
 
-app.get('/user/:id', async function (req, res) {
-  const { id } = req.params;
-  const user = await prisma.user.findUnique({
-    where: {
-      id: parseInt(id),
-    },
-  });
-  res.send(user);
-});
+// app.get('/users', async function (req, res) {
+//   const users = await prisma.user.findMany();
+//   res.send(users);
+// });
 
-app.post('/user/create', async function (req, res) {
-  const { user } = req.body;
-  const result = await prisma.user.create({
-    data: user,
-  });
-  res.send(result);
-});
+// app.get('/user/:id', async function (req, res) {
+//   const { id } = req.params;
+//   const user = await prisma.user.findUnique({
+//     where: {
+//       id: parseInt(id),
+//     },
+//   });
+//   res.send(user);
+// });
 
-app.post('/user/update', async function (req, res) {
-  const { user, id } = req.body;
-  const result = await prisma.user.update({
-    where: {
-      id: parseInt(id),
-    },
-    data: user,
-  });
-  res.send(result);
-});
+// app.post('/user/create', async function (req, res) {
+//   const { user } = req.body;
+//   const result = await prisma.user.create({
+//     data: user,
+//   });
+//   res.send(result);
+// });
 
-app.delete('/user/delete', async function (req, res) {
-  const { id } = req.body;
-  const result = await prisma.user.delete({
-    where: {
-      id: parseInt(id),
-    },
-  });
-  res.send(result);
-});
+// app.post('/user/update', async function (req, res) {
+//   const { user, id } = req.body;
+//   const result = await prisma.user.update({
+//     where: {
+//       id: parseInt(id),
+//     },
+//     data: user,
+//   });
+//   res.send(result);
+// });
 
-app.listen(port, () => {
-  console.log(`Server run at http://localhost:${port}`);
-});
+// app.delete('/user/delete', async function (req, res) {
+//   const { id } = req.body;
+//   const result = await prisma.user.delete({
+//     where: {
+//       id: parseInt(id),
+//     },
+//   });
+//   res.send(result);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server run at http://localhost:${port}`);
+// });
