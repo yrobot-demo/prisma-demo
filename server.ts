25c3dbbd3060b8bc22bbdea548f5dbbf2d30bc3b
Yrobot <yrobot@qq.com>
2021-07-19 17:31:52 +0800

    login finish

--- server.ts
+++ server.ts
@@ -1,26 +1,40 @@
 import 'reflect-metadata';
-import { buildSchema, Authorized, Resolver, Query, Ctx, Arg } from 'type-graphql';
-import { ApolloServer } from 'apollo-server';
+import { buildSchema, Authorized, Resolver, Query, Ctx, Arg, ObjectType, Field } from 'type-graphql';
+import { ApolloServer, AuthenticationError } from 'apollo-server';
 import { PrismaClient } from '@prisma/client';
 
 import { resolvers, ResolversEnhanceMap, applyResolversEnhanceMap, User } from '@generated/type-graphql';
 
+interface Context {
+  user: string;
+  prisma: PrismaClient;
+}
+
+@ObjectType()
+class UserWithToken {
+  @Field()
+  token: string;
+
+  @Field()
+  user: User;
+}
 @Resolver()
 export class LoginResolver {
-  @Query()
-  async login(@Ctx() { prisma }, @Arg('name') name, @Arg('password') password) {
+  @Query((returns) => UserWithToken)
+  async login(
+    @Ctx() { prisma }: Context,
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+  ): Promise<UserWithToken> {
     const result = await prisma.user.findUnique({
-      where: { name, password },
+      where: { email },
     });
-    if (result) {
+    if (result?.password === password)
       return {
+        user: result,
         token: 'token',
       };
-    } else {
-      return {
-        error: 'error',
-      };
-    }
+    throw new AuthenticationError('No such account or The password error');
   }
 }
 
@@ -34,7 +48,7 @@ async function main() {
   applyResolversEnhanceMap(resolversEnhanceMap);
 
   const schema = await buildSchema({
-    resolvers: resolvers,
+    resolvers: [LoginResolver, ...resolvers],
     validate: false,
     authChecker: ({ root, args, context, info }, roles) => {
       const user = context.user || '';
@@ -50,9 +64,9 @@ async function main() {
 
   const server = new ApolloServer({
     schema,
-    context: ({ req }) => {
+    context: ({ req }): Context => {
       const user = req.headers.authorization || '';
-      return { user, prisma, req };
+      return { user, prisma };
     },
   });
   const { port } = await server.listen(4000);
